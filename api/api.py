243089132060d58178069c5d969d695548b6b2a1
Angel-Padilla <angel.p.lopez@outlook.com>
2026-02-13 16:37:47 -0600

    feat: setup a `test` API endpoint for tests that can be performed from the dial app

--- api/api.py
+++ api/api.py
@@ -50,6 +50,7 @@ class API:
         from . import machine as _machine  # noqa
         from . import serial as _serial  # noqa
         from . import password_handler as _password_handler  # noqa
+        from . import tests as _tests  # noqa
 
         routes = []
         logger.info("API Routes registered:")
---
+++ api/tests.py
@@ -0,0 +1,60 @@
+from sounds import SoundPlayer
+
+from .base_handler import BaseHandler
+from .api import API, APIVersion
+
+from log import MeticulousLogger
+from enum import StrEnum
+
+from machine import Machine
+import subprocess
+
+logger = MeticulousLogger.getLogger(__name__)
+
+
+class HardwareTests(StrEnum):
+    SPEAKER = "speaker"
+
+
+class TestsHandler(BaseHandler):
+    async def get(self, test_name=None):  # noqa: C901
+        if not Machine.is_idle:
+            self.write(
+                {
+                    "error": "testing prohibited",
+                    "details": "testing is only allowed in idle machine",
+                }
+            )
+            self.set_status(403)
+            self.finish()
+            return
+        match test_name:
+            case HardwareTests.SPEAKER:
+                # set the volume to 50%
+                try:
+                    subprocess.run(
+                        ["pactl", "--", "set-sink-volume", "0", "50%"], check=True
+                    )
+                except Exception:
+                    logger.warning(
+                        "could not set the volume to 50%, please cover your ears"
+                    )
+                finally:
+                    if SoundPlayer.play_sound("speaker_test"):
+                        self.write({"status": "okay"})
+                        self.finish()
+                    else:
+                        self.set_status(404)
+                        self.write(
+                            {"error": "sound not found", "details": "speaker_test"}
+                        )
+                # return the volume to 70%
+                try:
+                    subprocess.run(
+                        ["pactl", "--", "set-sink-volume", "0", "70%"], check=True
+                    )
+                except Exception:
+                    logger.warning("could not set the volume to 70%")
+
+
+API.register_handler(APIVersion.V1, r"/test[/]*(.*)", TestsHandler)
